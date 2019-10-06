83ceab3a78bb524c79c0aced86345b2667f9499d
Tianhan Lu <tianhan.lu@colorado.edu>
2019-10-06 13:16:55 -0700

    add a script to run the tool over all files under a directory

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-set -x
-./scripts/run.sh src/test/java/benchmarks/Zuleger2011bound.java
-./scripts/run.sh src/test/java/benchmarks/BreakParagraphs.java
+# set -x
+# ./scripts/run.sh src/test/java/benchmarks/Zuleger2011bound.java
+# ./scripts/run.sh src/test/java/benchmarks/BreakParagraphs.java
+
+FILES="$1/*.java"
+for f in $FILES
+do
+  echo "Processing $f ..."
+  ./scripts/run.sh "$f"
+  echo "\n"
+  # take action on each file. $f store current file name
+done
